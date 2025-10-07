bf635519d35ae707707d99246dca8d7ca2e6c1e8
Aisha del Tio <adeltiodeprado@gmail.com>
2025-10-07 20:27:56 +0200

    Add training script and chatbot data files
    Added training.py for preprocessing intents and generating training data. Introduced intents.json for chatbot patterns and responses, and included serialized words and classes (words.pkl, classes.pkl). Removed empty chatbot.py file.

---
+++ training.py
@@ -0,0 +1,60 @@
+import random 
+import json 
+import pickle
+import numpy as np
+
+import nltk
+from nltk.stem import WordNetLemmatizer
+
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation
+from keras.optimizers import SGD
+
+lemmatizer = WordNetLemmatizer()
+
+intents = json.loads(open('intents.json').read())
+
+nltk.download('punkt_tab', quiet=True)
+nltk.download('wordnet', quiet=True)
+nltk.download('omw-1.4', quiet=True)
+
+words = []
+classes = []
+documents = []
+ignore_letters = ['?', '!', '.', ',']
+
+for intent in intents['intents']:
+    for pattern in intent['patterns']:
+        word_list = nltk.word_tokenize(pattern)
+        words.extend(word_list)
+        documents.append((word_list, intent['tag']))
+        if intent['tag'] not in classes:
+            classes.append(intent['tag'])   
+
+words = [lemmatizer.lemmatize(word.lower()) for word in words if word not in ignore_letters]
+words = sorted(set(words))  
+
+pickle.dump(words, open('words.pkl', 'wb'))
+pickle.dump(classes, open('classes.pkl', 'wb')) 
+
+training = []
+output_empty = [0] * len(classes)
+for document in documents:
+    bag = []
+    word_patterns = document[0]
+    word_patterns = [lemmatizer.lemmatize(word.lower()) for word in word_patterns]
+    for word in words:
+        bag.append(1) if word in word_patterns else bag.append(0)
+
+    output_row = list(output_empty)
+    output_row[classes.index(document[1])] = 1
+    training.append([bag, output_row])
+    
+random.shuffle(training)
+training = np.array(training, dtype=object) 
+# print(training)
+
+train_x = np.array(list(training[:, 0]))
+train_y = np.array(list(training[:, 1]))    
+    
+
